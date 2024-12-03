df42e747530dfcf18288ee832f0eb6806d7d565f
Khadim MBENGUE <84906741+Xaadim-Bamba@users.noreply.github.com>
2024-12-03 23:06:50 +0100

    CI generation fichier environment
    création dynamique fichier mock environment avant d'exécuter les tests ou de lancer le build.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//import { environment } from '../environments/environnement';
-import { environment } from '../environments/environnement.mock';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
