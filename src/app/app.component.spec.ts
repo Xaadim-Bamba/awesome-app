efdf30adaba907a28cf3f4b3a32f6b98e20a7285
Khadim MBENGUE <84906741+Xaadim-Bamba@users.noreply.github.com>
2024-12-20 14:46:38 +0100

    Migrate from Karma/Jasmine to Jest (#11)
    * chore(debs): install Jest
    
    * chore(test) : migrate from karma/jasmine to jest
    
    * refacto test pour composant principal
    
    * chore(test) Create separate command for coverage
    
    * chore(ci) : adapt unit test command for jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, HttpClientModule],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -15,16 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'awesome-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Hello, awesome-app');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('awesome-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, awesome-app'
+    );
   });
 });
